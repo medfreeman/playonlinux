746c8ea799b413cbead3aa31b3ab0e2abdfbcd04
Mehdi Lahlou <mehdi.lahlou@free.fr>
2014-10-08 01:59:42 +0200

    removed useless overrides and components from rogue squadron, moved prefix creation after cdrom check, change TITLE to reflect entry name

--- RogueSquadron3D/RogueSquadron3D.sh
+++ RogueSquadron3D/RogueSquadron3D.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Date : (2014-09-18 00:12)
-# Last revision : (2014-10-07 14:35)
+# Last revision : (2014-10-08 01:38)
 # Wine version used : 1.7.28
 # Distribution used to test : Ubuntu 14.04 Trusty x64 + Debian 7.0 Wheezy x64
 # Author : med_freeman
@@ -9,23 +9,21 @@
 [ "$PLAYONLINUX" = "" ] && exit 0
 source "$PLAYONLINUX/lib/sources"
  
-TITLE="Rogue Squadron 3D"
+TITLE="Star Wars : Rogue Squadron 3D"
 PREFIX="RogueSquadron3D"
+EDITOR="LucasArts / Factor 5"
+GAME_URL="http://www.starwars.com/games-apps"
 WINE_VERSION="1.7.28"
 WINE_ARCH="x86"
 AUTHOR="med_freeman"
  
 POL_GetSetupImages "http://files.playonlinux.com/resources/setups/$PREFIX/top.jpg" "http://files.playonlinux.com/resources/setups/$PREFIX/left.jpg" "$TITLE"
 POL_SetupWindow_Init
+POL_SetupWindow_SetID 2293
  
 POL_Debug_Init
  
-POL_SetupWindow_presentation "$TITLE" "LucasArts / Factor 5" "http://www.starwars.com/games-apps" "$AUTHOR" "$PREFIX"
- 
-POL_System_SetArch "$WINE_ARCH"
-POL_System_TmpCreate "$PREFIX"
-POL_Wine_SelectPrefix "$PREFIX"
-POL_Wine_PrefixCreate "$WINE_VERSION"
+POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
 
 # Check game cdrom 
 POL_SetupWindow_cdrom
@@ -36,18 +34,23 @@ _16BIT="$(cat /proc/sys/abi/ldt16)"
 if [ $? -eq 0 ] && [ "$_16BIT" = "0" ]; then
     POL_Call POL_Function_RootCommand "sudo sysctl -w abi.ldt16=1; exit"
 fi
+
+POL_System_SetArch "$WINE_ARCH"
+POL_System_TmpCreate "$PREFIX"
+POL_Wine_SelectPrefix "$PREFIX"
+POL_Wine_PrefixCreate "$WINE_VERSION"
  
 POL_Wine_WaitBefore "$TITLE"
 POL_Wine start /unix "$CDROM/setup.exe"
 POL_Wine_WaitExit "$TITLE"
  
 # Update 1.21 installation
-POL_SetupWindow_message "$TITLE : $(eval_gettext 'Installation of update 1.21')" "$TITLE"
+POL_SetupWindow_message "$TITLE : $(eval_gettext 'Installation of update 1.2')" "$TITLE"
 
 POL_SetupWindow_InstallMethod "DOWNLOAD,LOCAL"
 if [ "$INSTALL_METHOD" = "LOCAL" ]; then
     cd "$HOME"
-    POL_SetupWindow_browse "$(eval_gettext 'Please select the 1.21 update executable')" "$TITLE"
+    POL_SetupWindow_browse "$(eval_gettext 'Please select the 1.2 update executable')" "$TITLE"
     UPDATE_EXE="$APP_ANSWER"
 elif [ "$INSTALL_METHOD" = "DOWNLOAD" ]; then
     cd "$POL_System_TmpDir"
@@ -60,7 +63,6 @@ POL_Wine_WaitExit "$TITLE"
 
 # Dependencies
 POL_Call POL_Install_dsound
-POL_Wine_OverrideDLL "native" "dsound"
 
 # Install nGlide wrapper
 cd "$POL_System_TmpDir"
@@ -80,20 +82,13 @@ _EOFREG_
 regedit $regfile
 rm -f $regfile
 
-# Needed Overrides
-Set_Managed "Off"
-POL_Wine_X11Drv "Decorated" "N"
-POL_Wine_X11Drv "GrabFullscreen" "Y"
-# Menu glitches
-POL_Wine_Direct3D "StrictDrawOrdering" "enabled"
-# Joystick
+# Joystick fix
 Set_OS "win98"
 
+SHORTCUT="Rogue Squadron 3D"
 # Shortcuts
-POL_Shortcut "Rogue Squadron.exe" "$TITLE"
-POL_Shortcut "nglide_config.exe" "$TITLE - Graphic settings"
- 
-POL_SetupWindow_message "$(eval_gettext '$TITLE has been successfully installed.')"
+POL_Shortcut "Rogue Squadron.exe" "$SHORTCUT"
+POL_Shortcut "nglide_config.exe" "$SHORTCUT - Graphic settings"
  
 POL_System_TmpDelete
 POL_SetupWindow_Close
