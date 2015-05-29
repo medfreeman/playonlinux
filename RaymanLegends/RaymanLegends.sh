782941b3616569b761ec89ab130fb81df9b1e35c
Mehdi Lahlou <mehdi.lahlou@free.fr>
2015-05-29 22:07:04 +0200

    updated to wine 1.7.43, small fixes

--- RaymanLegends/RaymanLegends.sh
+++ RaymanLegends/RaymanLegends.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Date : (2014-09-24 01:30)
-# Last revision : (2014-11-22 20:12)
-# Wine version used : 1.7.14-imm32_bug35361
+# Last revision : (2014-05-27 02:19)
+# Wine version used : 1.7.43
 # Distribution used to test : Ubuntu 14.04 Trusty x64
 # Author : med_freeman (from GNU_Raziel RaymanOrigins)
 # Licence : Retail
@@ -16,11 +16,12 @@ PREFIX="RaymanLegends"
 EDITOR="Ubisoft"
 GAME_URL="http://rayman.ubi.com/legends/"
 AUTHOR="GNU_Raziel + med_freeman"
-WORKING_WINE_VERSION="1.7.14-imm32_bug35361" # Allows steam overlay to work without crash
+WINE_VERSION="1.7.43"
+WINE_ARCH="x86" # for uplay
 GAME_VMS="512"
  
 # Starting the script
-POL_GetSetupImages "http://files.playonlinux.com/resources/setups/RaymanLegends/top.jpg" "http://files.playonlinux.com/resources/setups/RaymanLegends/left.jpg" "$TITLE"
+POL_GetSetupImages "http://files.playonlinux.com/resources/setups/$PREFIX/top.jpg" "http://files.playonlinux.com/resources/setups/$PREFIX/left.jpg" "$TITLE"
 POL_SetupWindow_Init
 POL_SetupWindow_SetID 2283
  
@@ -30,11 +31,9 @@ POL_Debug_Init
 POL_SetupWindow_presentation "$TITLE" "$EDITOR" "$GAME_URL" "$AUTHOR" "$PREFIX"
  
 # Setting prefix path
+POL_System_SetArch "$WINE_ARCH"
 POL_Wine_SelectPrefix "$PREFIX"
- 
-# Downloading wine if necessary and creating prefix
-POL_System_SetArch "x86" # for uplay
-POL_Wine_PrefixCreate "$WORKING_WINE_VERSION"
+POL_Wine_PrefixCreate "$WINE_VERSION"
  
 # Choose between DVD and Digital Download version
 POL_SetupWindow_InstallMethod "STEAM_DEMO,DVD,STEAM,LOCAL"
@@ -107,8 +106,6 @@ fi
 if [ "$INSTALL_METHOD" != "STEAM_DEMO" ] && [ "$INSTALL_METHOD" != "STEAM" ]; then
         POL_Shortcut "Rayman Legends.exe" "$TITLE" "$TITLE.png" ""
 fi
-
-POL_SetupWindow_message "$(eval_gettext '$TITLE has been successfully installed.')"
  
 POL_SetupWindow_Close
 exit 0
